63a181cd34f1f4fc2106d41b4add17ae14d85138
Chris Henning <saturos@gmail.com>
2013-01-04 15:56:07 -0500

    reindent all and remove tabs

--- starsign.c
+++ starsign.c
@@ -7,106 +7,109 @@
  */ 
 
 void starsign_select() {
-	int month;
-	uint32_t BIRTHSIGN_ADDR = 0;
-	uint32_t JUMP_TO = 0;
+  int month;
+  uint32_t BIRTHSIGN_ADDR = 0;
+  uint32_t JUMP_TO = 0;
 
-	// Get ADOM version number
-	char *version = getenv("ADOM_VERSION");
-	int adom_version = 0;
+  // Get ADOM version number
+  char *version = getenv("ADOM_VERSION");
+  int adom_version = 0;
 
-	if (version != NULL)
-	{
-		sscanf(version, "%i", &adom_version);
-	}
+  if (version != NULL)
+  {
+    sscanf(version, "%i", &adom_version);
+  }
 
 
-	/*
-	   BIRTHSIGN_ADDR is the place in memory where the day of birth is stored.
-	   Its value in memory will be the in-game displayed value minus one.
+  /*
+     BIRTHSIGN_ADDR is the place in memory where the day of birth is stored.
+     Its value in memory will be the in-game displayed value minus one.
 
-	   JUMP_TO is where to resume execution in the ADOM executable.
+     JUMP_TO is where to resume execution in the ADOM executable.
 
-	   See http://www.adom.de/forums/showthread.php/1134-Choosing-star-sign?p=72882#post72882
-	   for information on finding these offsets.
-	 */
-	switch (adom_version) {
-		case 111:
-			BIRTHSIGN_ADDR = 0x82b61f0;
-			JUMP_TO = 0x813ee80;
-			break;
+     See http://www.adom.de/forums/showthread.php/1134-Choosing-star-sign?p=72882#post72882
+     for information on finding these offsets.
+   */
+  switch (adom_version) {
+    case 111:
+      BIRTHSIGN_ADDR = 0x82b61f0;
+      JUMP_TO = 0x813ee80;
+      break;
 
-		case 100:
-			BIRTHSIGN_ADDR = 0x82a66e4;
-			JUMP_TO = 0x0813ace0;
-			break;
+    case 100:
+      BIRTHSIGN_ADDR = 0x82a66e4;
+      JUMP_TO = 0x0813ace0;
+      break;
 
-		case 1203:
-			BIRTHSIGN_ADDR = 0x8283d40;
-			JUMP_TO = 0x081386d0;
-			break;
+    case 1203:
+      BIRTHSIGN_ADDR = 0x8283d40;
+      JUMP_TO = 0x081386d0;
+      break;
 
-		case 1204:
-			BIRTHSIGN_ADDR = 0x8285d40;
-			JUMP_TO = 0x08139fb0;
-			break;
+    case 1204:
+      BIRTHSIGN_ADDR = 0x8285d40;
+      JUMP_TO = 0x08139fb0;
+      break;
 
-		case 1205:
-			BIRTHSIGN_ADDR = 0x8286948;
-			JUMP_TO = 0x0813a860;
-			break;
-        	case 1206:
-                	BIRTHSIGN_ADDR = 0x828acbc;
-                	JUMP_TO = 0x0813bf70;
-                  break;
-          case 1207:
-                  BIRTHSIGN_ADDR = 0x82921fc;
-                  JUMP_TO = 0x81405a0;
-                  break;
-		default:
-			break;
-	}
+    case 1205:
+      BIRTHSIGN_ADDR = 0x8286948;
+      JUMP_TO = 0x0813a860;
+      break;
 
-	if ((BIRTHSIGN_ADDR == 0) || (JUMP_TO == 0)) {
-		printf("Don't know where to put the birth date or jump to.  Unknown ADOM version %i ?\n", adom_version);
-		return;
-	}
+    case 1206:
+      BIRTHSIGN_ADDR = 0x828acbc;
+      JUMP_TO = 0x0813bf70;
+      break;
 
-	do {
-		printf("\033[2J\033[1;1H");
+    case 1207:
+      BIRTHSIGN_ADDR = 0x82921fc;
+      JUMP_TO = 0x81405a0;
+      break;
 
-		printf("Select the month of your birth:\r\n"
-				"\r\n"
-				"  ? - random\r\n"
-				"\r\n"
-				"  A - Raven\r\n"
-				"  B - Book\r\n"
-				"  C - Wand\r\n"
-				"  D - Unicorn\r\n"
-				"  E - Salamander\r\n"
-				"  F - Dragon\r\n"
-				"  G - Sword\r\n"
-				"  H - Falcon\r\n"
-				"  I - Cup\r\n"
-				"  J - Candle\r\n"
-				"  K - Wolf\r\n"
-				"  L - Tree\r\n"
-				"\r\n"
-				"> ");
+    default:
+      break;
+  }
 
-		fflush(stdout);
-		month = toupper(fgetc(stdin));
-	} while(month != '?' && (month < 'A' || month > 'L'));
+  if ((BIRTHSIGN_ADDR == 0) || (JUMP_TO == 0)) {
+    printf("Don't know where to put the birth date or jump to.  Unknown ADOM version %i ?\n", adom_version);
+    return;
+  }
 
-	printf("\033[2J");
-	fflush(stdout);
+  do {
+    printf("\033[2J\033[1;1H");
 
-	month = month == '?' ? 0 : month-'A'+1;
+    printf("Select the month of your birth:\r\n"
+        "\r\n"
+        "  ? - random\r\n"
+        "\r\n"
+        "  A - Raven\r\n"
+        "  B - Book\r\n"
+        "  C - Wand\r\n"
+        "  D - Unicorn\r\n"
+        "  E - Salamander\r\n"
+        "  F - Dragon\r\n"
+        "  G - Sword\r\n"
+        "  H - Falcon\r\n"
+        "  I - Cup\r\n"
+        "  J - Candle\r\n"
+        "  K - Wolf\r\n"
+        "  L - Tree\r\n"
+        "\r\n"
+        "> ");
 
-	if(!month) month = rand() % 12;
-	else month--;
+    fflush(stdout);
+    month = toupper(fgetc(stdin));
+  } while(month != '?' && (month < 'A' || month > 'L'));
 
-	*((uint16_t*)BIRTHSIGN_ADDR) = (unsigned int)(30*month + (rand()%30));
-	((void(*)())JUMP_TO)();
+  printf("\033[2J");
+  fflush(stdout);
+
+  month = month == '?' ? 0 : month-'A'+1;
+
+  if(!month) month = rand() % 12;
+  else month--;
+
+  *((uint16_t*)BIRTHSIGN_ADDR) = (unsigned int)(30*month + (rand()%30));
+  ((void(*)())JUMP_TO)();
 }
 
